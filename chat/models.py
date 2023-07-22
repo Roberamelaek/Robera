6e08be8d82f4acdb3b41ea462b0a0d73ea1b5ff9
Roberamelaek <69109461+Roberamelaek@users.noreply.github.com>
2023-07-21 21:11:20 -0400

    Revert "added 70% persent of the functionality and the text to speech functio…"

--- chat/models.py
+++ chat/models.py
@@ -1,17 +1,3 @@
 from django.db import models
-import uuid
-from django.utils import timezone
 
-class TextToSpeech(models.Model):
-    user_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    text = models.TextField()
-    mp3_file = models.FileField(upload_to='mp3_files/', null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    mp3_count_today = models.PositiveIntegerField(default=0)
-    last_generated_at = models.DateTimeField(null=True, blank=True)
-    
-    # Add a new field for user's IP address
-    user_ip = models.GenericIPAddressField(null=True, blank=True)
-
-    def __str__(self):
-        return f"TextToSpeech object - ID: {self.user_id}"
+# Create your models here.
--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('home/',views.home, name="home"),
-    path('', views.text_to_mp3, name='text_to_mp3'),    path('/text_to_mp3/', views.text_to_mp3, name='text_to_mp3'),    
+    path('',views.home, name="home"),
 ]
